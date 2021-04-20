4af3e48509a6508737263b8b4436f6eecfd9f10f
Rune <runeg96@hotmail.com>
2021-04-20 11:24:34 +0200

    Fixed --vis for eval

--- utils/dataset_processing/evaluation.py
+++ utils/dataset_processing/evaluation.py
@@ -8,22 +8,20 @@ warnings.filterwarnings("ignore")
 from .grasp import GraspRectangles, detect_grasps
 
 
-def plot_output(fig, rgb_img, grasp_q_img, grasp_angle_img, depth_img=None, no_grasps=1, grasp_width_img=None):
+def plot_output(rgb_img, depth_img, grasp_q_img, grasp_angle_img, no_grasps=1, grasp_width_img=None):
     """
-    Plot the output of a network
-    :param fig: Figure to plot the output
+    Plot the output of a GG-CNN
     :param rgb_img: RGB Image
     :param depth_img: Depth Image
-    :param grasp_q_img: Q output of network
-    :param grasp_angle_img: Angle output of network
+    :param grasp_q_img: Q output of GG-CNN
+    :param grasp_angle_img: Angle output of GG-CNN
     :param no_grasps: Maximum number of grasps to plot
-    :param grasp_width_img: (optional) Width output of network
+    :param grasp_width_img: (optional) Width output of GG-CNN
     :return:
     """
     gs = detect_grasps(grasp_q_img, grasp_angle_img, width_img=grasp_width_img, no_grasps=no_grasps)
 
-    plt.ion()
-    plt.clf()
+    fig = plt.figure(figsize=(10, 10))
     ax = fig.add_subplot(2, 2, 1)
     ax.imshow(rgb_img)
     for g in gs:
@@ -31,13 +29,12 @@ def plot_output(fig, rgb_img, grasp_q_img, grasp_angle_img, depth_img=None, no_g
     ax.set_title('RGB')
     ax.axis('off')
 
-    if depth_img:
-        ax = fig.add_subplot(2, 2, 2)
-        ax.imshow(depth_img, cmap='gray')
-        for g in gs:
-            g.plot(ax)
-        ax.set_title('Depth')
-        ax.axis('off')
+    ax = fig.add_subplot(2, 2, 2)
+    ax.imshow(depth_img, cmap='gray')
+    for g in gs:
+        g.plot(ax)
+    ax.set_title('Depth')
+    ax.axis('off')
 
     ax = fig.add_subplot(2, 2, 3)
     plot = ax.imshow(grasp_q_img, cmap='jet', vmin=0, vmax=1)
@@ -50,8 +47,7 @@ def plot_output(fig, rgb_img, grasp_q_img, grasp_angle_img, depth_img=None, no_g
     ax.set_title('Angle')
     ax.axis('off')
     plt.colorbar(plot)
-    plt.pause(0.1)
-    fig.canvas.draw()
+    plt.show()
 
 
 def calculate_iou_match(grasp_q, grasp_angle, ground_truth_bbs, no_grasps=1, grasp_width=None, threshold=0.25):
